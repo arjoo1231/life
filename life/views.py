83b6a19f95944b75b6cec9b6147edb433e339a48
arjoo1231 <97974344+arjoo1231@users.noreply.github.com>
2023-10-02 06:04:06 -0700

    Update views.py

--- life/views.py
+++ life/views.py
@@ -2,4 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Hello world!')
+    return HttpResponse('Title: "My Life Reflections"
+
+One of the most important lessons I've learned is that individuals tend to prioritize their own interests and goals. This realization has emphasized the significance of self-care and the importance of focusing on my own path. Time, as I've discovered, is a precious resource that should not be wasted. It's a finite asset that requires careful investment in pursuits that bring me fulfillment and happiness.
+
+My life journey has taught me the power of self-awareness, honesty, and mindful time management. These insights have guided me on a path of personal growth and fulfillment, reminding me to make every moment count in this incredible journey called life.')
